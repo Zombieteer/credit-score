f5f0d50c598ab953cf732bb07a6fece660902f3d
zombieteer <aryan.nigam1996@gmail.com>
2023-12-22 00:33:34 +0530

    interceptors update

--- src/utils/createAxios.js
+++ src/utils/createAxios.js
@@ -1,56 +1,5 @@
 import axios from "axios";
 
-const getErrorsInArray = (errorResponse) => {
-  let errors = [];
-
-  if (
-    Array.isArray(errorResponse.data) &&
-    typeof errorResponse.data[0].message === "string"
-  ) {
-    //fastest-validator error format
-    // console.log("fastest-validation");
-    errorResponse.data.forEach((error) => {
-      errors.push(error.message);
-    });
-  } else if (
-    Array.isArray(errorResponse.data.error) &&
-    typeof errorResponse.data.error[0] === "string"
-  ) {
-    //manual array format error
-    // console.log("manual array");
-    errorResponse.data.error.forEach((error) => {
-      errors.push(error);
-    });
-  } else if (
-    Array.isArray(errorResponse.data.error) &&
-    typeof errorResponse.data.error[0].details[0].message === "string"
-  ) {
-    //joi error validation
-    // console.log("joi array");
-    errors.push(errorResponse.data.error[0].details[0].message);
-  } else if (typeof errorResponse.data.error === "string") {
-    //manual error contain only one object
-
-    // console.log("manual one object");
-    errors.push(errorResponse.data.error);
-  } else if (typeof errorResponse.data.message === "string") {
-    //manual error contain only one object
-    // console.log("custom error ars");
-    errors.push(errorResponse.data.message);
-  } else if (typeof errorResponse.data.detail === "string") {
-    //manual error contain only one object
-    // console.log("custom error ars");
-    errors.push(errorResponse.data.detail);
-  } else errors.push("Something Went Wrong");
-  return errors;
-};
-
-const displayErrors = (errorResponse) => {
-  if (errorResponse?.status >= 400 && errorResponse?.status <= 500) {
-    return getErrorsInArray(errorResponse);
-  } else return [];
-};
-
 const createAxios = () => {
   const config = {};
   config.baseURL = "http://18.183.153.156:8000";
